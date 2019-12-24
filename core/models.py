a41a1a8913d65a4931fd3a508f41e13f7b05d7f1
karinka22149 <karina.nurieva2000@gmail.com>
2019-12-24 16:37:08 +0000

    commit

--- core/models.py
+++ core/models.py
@@ -38,7 +38,7 @@ class Staff(models.Model):
         return self.get_full_name()
 
 class Realization(models.Model):
-    service = models.ForeignKey('Service', on_delete=models.CASCADE, blank=True, null=True, verbose_name='Услуга')
+    service = models.ForeignKey('Service', on_delete=models.CASCADE, blank=True, null=True, verbose_name='')
     check_number = models.PositiveIntegerField(unique=True, verbose_name='Номер чека')
     staff = models.ForeignKey(Staff, on_delete=models.CASCADE, blank=True, null=True, verbose_name='Сотрудник')
     date = models.DateTimeField(auto_now_add=True, verbose_name='Время и дата')
