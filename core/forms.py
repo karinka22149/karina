44bc7a3a55fde03b106eff960028fa51cc59bde7
karinka22149 <karina.nurieva2000@gmail.com>
2019-12-24 18:10:14 +0000

    commit

--- core/forms.py
+++ core/forms.py
@@ -35,13 +35,7 @@ class RealizationForm(forms.ModelForm):
         fields = '__all__'
 
 class StaffForm(forms.ModelForm):
-    FIO = forms.ModelChoiceField(queryset=Staff.objects.all(), widget=forms.HiddenInput)
-    table_number = forms.ModelChoiceField(queryset=Staff.objects.all(), widget=forms.HiddenInput)
-    telephone_number = forms.ModelChoiceField(queryset=Staff.objects.all(), widget=forms.HiddenInput)
-    clothing_size = forms.ModelChoiceField(queryset=Staff.objects.all(), widget=forms.HiddenInput)
-    position = forms.ModelChoiceField(queryset=Staff.objects.all(), widget=forms.HiddenInput)
-    user = forms.ModelChoiceField(queryset=Staff.objects.all(), widget=forms.HiddenInput)
 
     class Meta:
-        model = StudentAnswer
-        fields = ['FIO', 'table_number', 'telephone_number', 'clothing_size','position','user']
+        model = Staff
+        fields = '__all__'
--- core/views.py
+++ core/views.py
@@ -110,16 +110,20 @@ class StaffView(View):
     
     def get(self, request, *args, **kwargs):
         form = self.form_class
+        staff = Staff.objects.all()
         context = {
-            'form': form
-        return render(request, self.template_name)
+            'form': form,
+            'staff' : staff
+        }
+        return render(request, self.template_name, context)
     
     def post(self, request, *args, **kwargs):
         form = self.form_class(request.POST)
         if form.is_valid():
             form.save()
         context = {
-            'form': form
+            'form': form,
+        }
         return render(request, self.template_name)
 
 class ReciepeView(View):
